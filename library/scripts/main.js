4b9450c0f57adc0d7d7a980654040acddca42bb0
AkiraQZ <vboyrkin@gmail.com>
2023-08-14 03:13:59 +0800

    feat: add aside menu script

---
+++ library/scripts/main.js
@@ -0,0 +1,10 @@
+if (1>0){
+document.addEventListener('DOMContentLoaded', function() {
+   document.getElementById('burger').addEventListener('click', function() {
+    document.querySelector('header').classList.toggle('open')
+   document.getElementById('cross').addEventListener('click', function() {
+    document.querySelector('header').classList.toggle('open')
+   })
+   })
+})
+}
